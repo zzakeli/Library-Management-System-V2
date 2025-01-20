2a8e9e4d9eff8e640410a7091bb62cd7857692a1
Jzy <jestalycastillo15@gmail.com>
2025-01-20 16:04:44 +0800

    Initialized furthermore

---
+++ Library-Management-System-V2/src/Functions.java
@@ -0,0 +1,7 @@
+public abstract class Functions {
+    public abstract void add();
+
+    public abstract void delete();
+
+    public abstract void edit();
+}
---
+++ Library-Management-System-V2/src/INITIAL/Constants.java
@@ -0,0 +1,5 @@
+package INITIAL;
+
+public class Constants {
+
+}
